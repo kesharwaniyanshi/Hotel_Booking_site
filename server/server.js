6b0560e29dd380fcd83798104e70695d371cdd8a
kesharwaniyanshi <kesharwaniyanshi@gmail.com>
2024-06-25 23:01:00 +0530

    vercel json added

--- server/server.js
+++ server/server.js
@@ -16,6 +16,7 @@ const errorMiddleware = require('./middlewares/error-middleware');
 
 // Tackle cors
 const corsOptions = {
+    // origin: 'http://localhost:5173',
     origin: 'http://localhost:5173',
     methods: "GET,POST,PUT,PATCH,DELETE",
     credentials: true,
